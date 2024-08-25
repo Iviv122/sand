846dc9bab7842bac6beb3b8f9a66c23ad1b4a7a7
IvanSkrynnik <ivan.skrinnik.nomia@gmail.com>
2024-08-25 11:11:40 +0200

    Mouse kinda work

--- main.c
+++ main.c
@@ -6,7 +6,9 @@
 #define ROWS 500
 #define COLS 500
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+
 
 float scale = (float)1/ROWS;
 int** matrix;
@@ -25,7 +27,7 @@ matrix[i] = (int*)calloc(ROWS,sizeof(int));
     if (!glfwInit())
         return -1;
 
-    window = glfwCreateWindow(1000, 1000, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(500, 500, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -34,8 +36,8 @@ matrix[i] = (int*)calloc(ROWS,sizeof(int));
 	setSand(matrix,0,0);
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-	glfwSetKeyCallback(window, key_callback);
-    /* Loop until the user closes the window */
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+/* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
@@ -43,15 +45,14 @@ matrix[i] = (int*)calloc(ROWS,sizeof(int));
 
 	
 	glLoadIdentity();
-	glScalef(scale,scale,scale);
+	glScalef(scale*2,scale*2,scale*2);
 	
 	glBegin(GL_POINTS);
 	for(int y=ROWS-1;y>=0;--y){
 	for(int x=0;x<COLS;++x){
-	
 	if(matrix[x][y]==1){
 	updateSand(matrix,x,y);
-	glVertex2f(x-(COLS/2),y-(ROWS/2));
+	glVertex2f(x-(ROWS/2),y-(COLS/2));
 	}
 	}
 	}
@@ -71,9 +72,17 @@ matrix[i] = (int*)calloc(ROWS,sizeof(int));
     glfwTerminate();
     return 0;
 }
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 {
-    if (key == GLFW_KEY_SPACE && action == GLFW_REPEAT){
-	setSand(matrix,249,300);
-	}
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS){	
+	double xpos,ypos;
+	glfwGetCursorPos(window, &xpos, &ypos);
+	
+	int X = (int)xpos;
+	int Y = (int)-ypos;
+	printf("x: %f y: %f\n",xpos,ypos);
+	printf("x: %d y: %d\n\n",X,Y);
+	setSand(matrix,X,Y);
+}
 }
